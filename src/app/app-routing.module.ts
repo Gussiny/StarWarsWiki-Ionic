31bd7fea244f1ec88995306df7f762977a9c0b89
Gustavo Flores <37113826+Gussiny@users.noreply.github.com>
2020-05-14 22:30:16 -0500

    feat: todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,26 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'planets',
+    loadChildren: () => import('./pages/planets/planets.module').then( m => m.PlanetsPageModule)
+  },
+  {
+    path: 'people',
+    loadChildren: () => import('./pages/people/people.module').then( m => m.PeoplePageModule)
+  },
+  {
+    path: 'starships',
+    loadChildren: () => import('./pages/starships/starships.module').then( m => m.StarshipsPageModule)
+  },
+  {
+    path: 'planets/:id',
+    loadChildren: () => import('./pages/description-planets/description-planets.module').then( m => m.DescriptionPlanetsPageModule)
+  },
+  {
+    path: 'people/:id',
+    loadChildren: () => import('./pages/description-people/description-people.module').then( m => m.DescriptionPeoplePageModule)
   }
 ];
 @NgModule({
